c1a26bb5da9d2c230663c8051371f5c5ac884274
hanochk <hanoch.kremer@gmail.com>
2023-06-23 18:13:40 +0000

    image contrastive fine tune of 1 iteration

--- gender_classification.py
+++ gender_classification.py
@@ -31,7 +31,7 @@ class Data(Dataset):
         self.is_classifier_uniq_cls = kwargs.pop('classifier_uniq_cls', False)
         if self.is_classifier_uniq_cls:
             self.classes = np.unique(text_list)
-            self.classifier_uniq_cls_tokens = tokenizer(["A photo of a {}".format(x) for x in self.classes])
+            self.classifier_uniq_cls_tokens = tokenizer(["A photo of a {}".format(x) for x in self.classes]) #[49406,   320,  1125,   539,   320,  2801, 49407] a=320 |man=2801
 
         assert(len(self.image_path) == len(self.text_tokens))
         return
@@ -43,11 +43,14 @@ class Data(Dataset):
         if self.is_classifier_uniq_cls:
             image = self.img_preprocess(Image.open(self.image_path[idx]))
             label = np.where(self.classes == self.class_text[idx])[0].item()
+            # text = self.text_tokens[idx]
+            # print(idx, self.class_text[idx])
             return image, label, torch.tensor([-1])
         else:
             image = self.img_preprocess(Image.open(self.image_path[idx]))
             text = self.text_tokens[idx]
-            label = [0 if self.class_text[idx] == 'female' else 1] # 0 -feamle 1- male no offence :-)
+            label = [0 if self.class_text[idx] == 'female' else 1][0] # 0 -feamle 1- male no offence :-)
+            # print(self.class_text[idx])
             return image, label, text
 
 # pip install git+https://github.com/openai/CLIP.git
@@ -55,9 +58,11 @@ class ClipVideoUtils:
     def __init__(self, device, batch_size:int =1024, model_name:str ='RN50x4'): 
         """
         The optimal batch size depends on the GPU. If CPU, it's zero
+        RN50x4 model expects an 288x288 image input        
         """
         self.device = device
         self.model, self.preprocess = clip.load(model_name, device=self.device)
+        self.tokenizer = clip.tokenize
         self.batch_size = batch_size if torch.cuda.is_available() else 0
         self._convert_weights_back_to_fp16 = clip.model.convert_weights
     
@@ -77,69 +82,67 @@ class ClipVideoUtils:
     def val_dataset(self, a):
         self._val_dataset = a
 
-    def train_model_contrastive(self, dataloader: Data, loss_img, optimizer, num_epochs: int=1, **kwargs):
-        
-        #https://github.com/openai/CLIP/issues/57
-        def convert_models_to_fp32(model): 
-            for p in model.parameters(): 
-                p.data = p.data.float() 
-                if p.grad != None:
-                    p.grad.data = p.grad.data.float() 
-
-        max_iter = kwargs.pop('max_iterations', -1)
+    def train_model_contrastive(self, dataloader: Data, loss_img, optimizer, num_epochs: int=1, **kwargs): ##(self, dataloader: Data, **kwargs):
         all_targets = list()
         all_simillarity = list()
         all_predictions = list()
-        epoch_loss = list()
+        max_iter = kwargs.pop('max_iterations', -1)
+        
         self.model.train()
-
         for epoch in range(num_epochs):
             running_loss = 0
             print('Epoch {}/{}'.format(epoch, num_epochs - 1))
             print('-' * 10)
-            # TODO eval process after each epoch
             for iters, (images, labels, texts) in enumerate(tqdm.tqdm(dataloader)):
-
-                if (max_iter != -1 and iters > max_iter):
+                if (max_iter != -1 and iters > max_iter-1):
                     break
-                optimizer.zero_grad()                
+
+                optimizer.zero_grad() 
                 image_input = to_device(images, self.device)
-    # text_inputs = to_device(clip.tokenize(["A photo of a {}".format(x) for x in text]), self.device)
-                if dataloader.dataset.is_classifier_uniq_cls:
-                    text_features = self.model.encode_text(to_device(dataloader.dataset.classifier_uniq_cls_tokens, self.device))             # TODO : avoid re-encode pre-defined texts just read from local tensor
+    # text is already tokenized  :text_inputs = to_device(clip.tokenize(["A photo of a {}".format(x) for x in text]), self.device) =># 
+                if dataloader.dataset.is_classifier_uniq_cls: 
+                    text_features = self.model.encode_text(to_device(dataloader.dataset.classifier_uniq_cls_tokens, self.device))             # TBD : avoid re-encode pre-defined texts just read from local tensor
                 else:
                     text_inputs = to_device(texts, self.device)
-                    text_features = self.model.encode_text(text_inputs)             # TODO : avoid re-encode pre-defined texts just read from local tensor
-# 
-                # cosine similarity as logits forward method()   #https://github.com/openai/CLIP/blob/a9b1bf5920416aaeaec965c25dd9e8f98c864f16/clip/model.py#LL366C9-L366C38
+                    text_features = self.model.encode_text(text_inputs)             # TBD : avoid re-encode pre-defined texts just read from local tensor
+
                 image_features = self.model.encode_image(image_input)
-                # normalized features
-                image_features = image_features / image_features.norm(dim=1, keepdim=True)
-                text_features = text_features / text_features.norm(dim=1, keepdim=True)
-
-                # cosine similarity as logits
-                logit_scale = 1 #self.model.logit_scale.exp() # temperature should stay low for bin classifier
-                logits_per_image = logit_scale * image_features @ text_features.t()
-                # logits_per_text = logits_per_image.t()
+                if 0:
+                    image_features_norm = image_features # normalization is not mandatory for argmax()
+                    text_features_norm = text_features
+                else:
+                    image_features_norm = image_features/image_features.norm(dim=-1, keepdim=True) # normalization is not mandatory for argmax()
+                    text_features_norm = text_features/text_features.norm(dim=-1, keepdim=True)
                 
-                ground_truth = to_device(labels[0], self.device).long() #torch.arange(len(images),dtype=torch.long, device=self.device)
+                similarity = (image_features_norm @ text_features_norm.T)
 
-                total_loss = loss_img(logits_per_image, ground_truth)
+                ground_truth = to_device(labels, self.device).long() 
+                total_loss = loss_img(similarity, ground_truth)
                 total_loss.backward()
-                if self.device == "cpu" or 1:
-                    optimizer.step()
-                else: 
-                    convert_models_to_fp32(self.model)
-                    optimizer.step()
-                    self._convert_weights_back_to_fp16(self.model)
-# 
-        # Hidden assumption: The loss has a "mean" reduction (see pytorch loss arguments)
+                optimizer.step()
                 running_loss += total_loss.item() * dataloader.batch_size
+                # Training acc in batch
+                max_sim_ind = np.argmax(similarity.detach().cpu().numpy(), axis=1)
+                acc =  np.where(max_sim_ind-labels.numpy() == 0)[0].shape[0]/labels.shape[0]
+                print(acc)
+
+                all_simillarity.append(similarity.detach().cpu().numpy())
+                all_predictions.append(max_sim_ind)
+                all_targets.append(labels)
                 print("total_loss", total_loss)
                 print("running_loss", running_loss)
+
             print(running_loss / len(dataloader.dataset))
-            epoch_loss.append(running_loss / len(dataloader.dataset))
-        return epoch_loss ,running_loss
+            # max_sim_ind = np.argmax(similarity.detach().cpu().numpy(), axis=1)
+            # acc =  np.where(max_sim_ind-labels.numpy() == 0)[0].shape[0]/labels.shape[0]
+            # print(acc)
+
+        all_targets = np.concatenate(all_targets)
+        all_predictions = np.concatenate(all_predictions)
+        all_simillarity = np.concatenate(all_simillarity)
+
+
+        return all_targets, all_predictions, all_simillarity
 
     def eval_model_contrastive(self, dataloader: Data, **kwargs):
         all_targets = list()
@@ -151,8 +154,8 @@ class ClipVideoUtils:
         with torch.no_grad():
             for images, targets, texts in tqdm.tqdm(dataloader):
                 image_input = to_device(images, self.device)
-    # text_inputs = to_device(clip.tokenize(["A photo of a {}".format(x) for x in text]), self.device)
-                if dataloader.dataset.is_classifier_uniq_cls:
+    # text is already tokenized  :text_inputs = to_device(clip.tokenize(["A photo of a {}".format(x) for x in text]), self.device) =># 
+                if dataloader.dataset.is_classifier_uniq_cls: 
                     text_features = self.model.encode_text(to_device(dataloader.dataset.classifier_uniq_cls_tokens, self.device))             # TBD : avoid re-encode pre-defined texts just read from local tensor
                 else:
                     text_inputs = to_device(texts, self.device)
@@ -176,76 +179,8 @@ class ClipVideoUtils:
             return all_targets, all_predictions, all_simillarity
 
 
-    # def eval_model_cusstome(self, loss_fn=None, **kwargs):
-    #     self.model.eval()
-
-    #     with torch.no_grad():
-    #         for read_list in tqdm.tqdm(dataloader):
-    #             # TODO: NORMALIZATION! In case we add something else than hardcoded normalization
-    #             # dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size,
-    #             #                                          shuffle=False)
-    #             # Check if model in cuda : next(model.parameters()).is_cuda  => should be True
-    #             total_val_loss = 0.0
-    #             n_samples = 0
-
-    #             all_targets = list()
-    #             all_predictions = list()
-    #             all_features = list()
-    #             batch_counter = 0
-    #             all_tile_id = list()
-    #             all_atten_weithgs = list()
-    #             all_tile_index_pos = list()
-
-    #             for read_list in tqdm.tqdm(dataloader):
-    #                 inputs, targets = read_list
-    #                 inputs = to_device(inputs, device)
-
-    #                 if kwargs['extract_features']:
-    #                     if hasattr(model, '_custom_forward_impl'): # model that can combines Hand HCF should have this method implemented to combine HCF
-    #                         x = model.features(inputs) # running model double time, didn;t find a way to resue features
-    #                         features = nn.functional.adaptive_avg_pool2d(x, 1).reshape(x.shape[0], -1)  # copied rfom mobilenet.py
-    #                         predictions = model.forward(inputs)
-    #                     else:
-    #                         x = model.features(inputs[0])
-    #                         features = nn.functional.adaptive_avg_pool2d(x, 1).reshape(x.shape[0], -1) # copied rfom mobilenet.py
-    #                         predictions = model.forward(inputs[0])
-
-    #                     # predictions, features = model.forward_return_multiple_features(inputs)
-    #                 else:
-    #                     if hasattr(model, '_custom_forward_impl') or hasattr(model, '_custom_forward_impl_embeddings_pooling'): # model that can combines Hand HCF should have this method implemented to combine HCF
-    #                         predictions = model.forward(inputs)
-    #                     else:
-    #                         predictions = model.forward(inputs[0])
-
-    #                     features = None
-
-    #                 if loss_fn is not None:
-    #                     val_loss = loss_fn(predictions, targets)
-    #                     n_samples_in_batch = len(targets)
-    #                     n_samples += n_samples_in_batch
-    #                     # Hidden assumption: The loss has a "mean" reduction (see pytorch loss arguments)
-    #                     total_val_loss += val_loss.item() * n_samples_in_batch
-
-    #                 if do_softmax:
-    #                     predictions = torch.nn.functional.softmax(predictions, dim=1)
-    #                     # print(predictions[:,1])
-    #                 else:
-    #                     predictions = predictions # logits
-
-    #             all_targets.append(targets.cpu().numpy())
-    #             all_predictions.append(predictions.cpu().numpy())
-
-
-    #             batch_counter += 1
-    #             if batch_counter == max_number_of_batches:
-    #                 break
-    #         all_targets = np.concatenate(all_targets)
-    #         all_predictions = np.concatenate(all_predictions)
-    #     loss = total_val_loss / n_samples if loss_fn is not None else None
-
-
-    
-def data_factory(img_preprocess, val_image_path: str, train_image_path: str, batch_size: int, num_workers: int):
+   
+def data_factory(img_preprocess, val_image_path: str, train_image_path: str, batch_size: int, num_workers: int, tokenizer):
 
     train_image_fname = glob.glob(train_image_path + '/**/*.jpg', recursive=True) + glob.glob(train_image_path + '/**/*.png', recursive=True) + \
                     glob.glob(train_image_path + '/**/*.jpeg', recursive=True)
@@ -257,10 +192,10 @@ def data_factory(img_preprocess, val_image_path: str, train_image_path: str, bat
 
     val_image_text = [x.split('/')[-2] for x in val_image_fname]
 
-    train_dataset = Data(img_preprocess=img_preprocess, tokenizer=clip.tokenize, 
-                                image_path=train_image_fname, text_list=train_image_text, classifier_uniq_cls=False) # take the 
+    train_dataset = Data(img_preprocess=img_preprocess, tokenizer=tokenizer, 
+                                image_path=train_image_fname, text_list=train_image_text, classifier_uniq_cls=True) 
     
-    val_dataset = Data(img_preprocess=img_preprocess, tokenizer=clip.tokenize, 
+    val_dataset = Data(img_preprocess=img_preprocess, tokenizer=tokenizer, 
                                 image_path=val_image_fname, text_list=val_image_text, classifier_uniq_cls=True)
 
     train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
@@ -277,7 +212,7 @@ def data_factory(img_preprocess, val_image_path: str, train_image_path: str, bat
 def main():
     # Params to be moved to argsparse/user_defined 
     batch_size = int(128/8) # to be optimized with respect to GPU
-    num_workers = 8
+    num_workers = int(8/4)
     result_dir = '/notebooks/multi_modal'
     state_file_name = 'clip_gender_ft_cls.pt'
     eval_at_1st = False
@@ -295,31 +230,34 @@ def main():
                                                     val_image_path=val_image_path, 
                                                     train_image_path=train_image_path,
                                                     batch_size=batch_size,
-                                                    num_workers=num_workers)
+                                                    num_workers=num_workers,
+                                                    tokenizer=clip_obj.tokenizer)
 
     if eval_at_1st:
-        all_targets, all_predictions, all_simillarity = clip_obj.eval_model_contrastive(dataloader=val_dataloader)
+        all_targets, all_predictions, all_simillarity = clip_obj.eval_model_contrastive(dataloader=val_dataloader) 
         acc =  np.where(all_targets-all_predictions == 0)[0].shape[0]/all_targets.shape[0]# TODO consider replace with metrics.auc thresholding over the similarity score !!!
         print("ACC: {} over validation set {} images".format(acc, all_targets.shape[0]))
         if not np.isclose(acc, 0.9672933298995622):
-            raise ValueError("something wrong with model or data has changed ")
+            print("something wrong with model or data has changed ")
         # FT model wit hmoderate Lr
     loss_img = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(clip_obj.model.parameters(), lr=1e-5, weight_decay=0.001) #Params used from paper, the lr is smaller, more safe for fine tuning to new dataset
-    running_loss = clip_obj.train_model_contrastive(dataloader=train_dataloader,optimizer=optimizer, 
-                                                    loss_img=loss_img, max_iterations=1000)
+    optimizer = optim.AdamW(clip_obj.model.parameters(), lr=1e-7, betas=(0.9,0.98),eps=1e-6, weight_decay=0.0001) # the lr is smaller, more safe for fine tuning to new dataset
 
-    torch.save({
-        'epoch': -1,
-        'model_state_dict': clip_obj.model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'loss': running_loss,
-        },os.path.join(result_dir, state_file_name))
+    running_loss = clip_obj.train_model_contrastive(dataloader=train_dataloader,optimizer=optimizer, 
+                                                    loss_img=loss_img, max_iterations=1)
 
     all_targets, all_predictions, all_simillarity = clip_obj.eval_model_contrastive(dataloader=val_dataloader)
     acc =  np.where(all_targets-all_predictions == 0)[0].shape[0]/all_targets.shape[0]# TODO consider replace with metrics.auc thresholding over the similarity score !!!
     print("ACC: {} after FT over validation set {} images".format(acc, all_targets.shape[0]))
+    if not np.isclose(acc, 0.9695):
+        print("something wrong with model or data has changed ")
 
+    torch.save({ 
+        'epoch': -1, 
+        'model_state_dict': clip_obj.model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': running_loss,
+        },os.path.join(result_dir, state_file_name))
 
     return
 
@@ -327,8 +265,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# RN50x4
-# RN50x4
-# https://github.com/openai/CLIP/issues/83
 
-# RN50x4 model expects an 288x288 image input
+
